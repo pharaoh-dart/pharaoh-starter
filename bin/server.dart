a069d910eb377ede77b5c88a722da9859c21ab15
Chima Precious <ositachima.co@gmail.com>
2023-12-09 22:15:17 +0000

    view rendering example

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,22 @@
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:server/index.dart';
 
-void main(List<String> arguments) async {
+void main(args) {
+  makeIsolates(4);
+}
+
+void runServer(message) async {
   final app = createApp();
 
-  await app.listen();
+  await app.listen(port: 80);
+}
+
+Future<void> makeIsolates(int number) async {
+  for (int i = 0; i < number; i++) {
+    await Isolate.spawn(runServer, 'App Instance $i');
+  }
+
+  stdin.readByteSync();
 }
--- lib/index.dart
+++ lib/index.dart
@@ -1,11 +1,37 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:jinja/loaders.dart';
 import 'package:pharaoh/pharaoh.dart';
+import 'package:jinja/jinja.dart';
+
+var source = '''
+            <div>{{ lastname }}, {{ firstname }}</div>	 ''';
 
 Pharaoh createApp() {
   final app = Pharaoh();
 
-  app.use(logRequests);
+  final env = Environment(
+    autoReload: false,
+    trimBlocks: true,
+    leftStripBlocks: true,
+    loader: FileSystemLoader(paths: ['resources/views'], extensions: {'j2'}),
+  );
+  app.viewEngine = JinjaViewEngine(env, fileExt: 'j2');
 
-  app.get('/', (req, res) => res.ok('Hello World 🚀'));
+  app.get('/', (req, res) async {
+    return res.render(
+      'index',
+      {'firstname': 'Chima', 'lastname': 'Precious'},
+    );
+  });
+
+  /// json -> is slow prolly use isolate
+  ///
+  /// hello world -> 2 isolates -> 90k Req/sec
+  ///
+  /// hello world -> 3 isolates -> 103k Req/sec
+  ///
 
   return app;
 }
